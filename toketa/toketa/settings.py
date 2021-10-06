b0b1bab430e5eedfd22779c3adcf1974e38138ec
Kaisei <kaisei.2482480501@icloud.com>
2021-10-06 15:40:00 +0900

    delete toketas app

--- toketa/toketa/settings.py
+++ toketa/toketa/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 自分のアプリケーション
+    'toketas',
+    # デフォルトのDjangoアプリケーション
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- toketa/toketas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ToketasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'toketas'
